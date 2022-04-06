e623b83c6f6437fed74c76278409d0e812274e30
lesterhnu <lesterhnu@gmail.com>
2022-04-06 20:57:51 +0800

    泛型测试

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,17 @@
+use std::ops::Add;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-pub fn get_current_unix() -> u64{
+pub fn get_current_unix() -> u64 {
     SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .expect("get_current_unix_err").as_secs()
 }
-pub fn add(x: i32, y: i32) -> i32 {
+
+pub fn add<T:Add<Output=T>>(x: T, y: T) -> T {
     x + y
+}
+
+#[test]
+pub fn testadd() {
+    assert_eq!(add(1, 2), 3);
 }
